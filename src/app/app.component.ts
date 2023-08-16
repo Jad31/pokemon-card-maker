4ad91b79e5062e49648c6f1afa6a178a59e3a69c
Jad Chahed <jad.chahed@bashroom.com>
2023-08-16 17:09:39 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
+
+@Component({
+  standalone: true,
+  imports: [NxWelcomeComponent, RouterModule],
+  selector: 'team-booster-pack-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'team-booster-pack';
+}
