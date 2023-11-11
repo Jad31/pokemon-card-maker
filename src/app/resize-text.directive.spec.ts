a489095fb99c224bd626841869e9721cf349f5e2
Jad Chahed <jad.chahed@bashroom.com>
2023-11-11 22:40:42 +0100

    feat: name resize based on its width

---
+++ src/app/resize-text.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ResizeTextDirective } from './resize-text.directive';
+
+describe('ResizeTextDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ResizeTextDirective();
+    expect(directive).toBeTruthy();
+  });
+});
